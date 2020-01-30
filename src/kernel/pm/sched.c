6d380ae4d6a2cec89963deea3fa2353e7e488905
Alexandre SALMON <salmon.alexandre10@gmail.com>
2020-01-30 10:50:56 +0100

    2 queues with round-robin

--- src/kernel/pm/sched.c
+++ src/kernel/pm/sched.c
@@ -24,6 +24,14 @@
 #include <nanvix/pm.h>
 #include <signal.h>
 
+struct multiqueue {
+	struct process *super_user_proc[PROC_MAX];
+	struct process *user_proc[PROC_MAX];
+	unsigned nb_su_proc;
+	unsigned nb_u_proc;
+};
+
+
 /**
  * @brief Schedules a process to execution.
  * 
@@ -87,32 +95,74 @@ PUBLIC void yield(void)
 	}
 
 	/* Choose a process to run next. */
-	next = IDLE;
-	for (p = FIRST_PROC; p <= LAST_PROC; p++)
-	{
-		/* Skip non-ready process. */
-		if (p->state != PROC_READY)
-			continue;
+	// next = IDLE;
+	// for (p = FIRST_PROC; p <= LAST_PROC; p++)
+	// {
+	// 	/* Skip non-ready process. */
+	// 	if (p->state != PROC_READY)
+	// 		continue;
 		
-		/*
-		 * Process with lowest
-		 * priority + niceness found
-		 * if equal higest waiting time
-		 * is choosen
-		 */
-		if (p->counter > next->counter)   
-		{
-			next->counter++;
-			next = p;
-		}
+	// 	/*
+	// 	 * Process with lowest
+	// 	 * priority + niceness found
+	// 	 * if equal higest waiting time
+	// 	 * is choosen
+	// 	 */
+	// 	if (p->counter > next->counter)   
+	// 	{
+	// 		next->counter++;
+	// 		next = p;
+	// 	}
 			
-		/*
-		 * Increment waiting
-		 * time of process.
-		 */
-		else
-			p->counter++;
+	// 	/*
+	// 	 * Increment waiting
+	// 	 * time of process.
+	// 	 */
+	// 	else
+	// 		p->counter++;
+	// }
+
+	struct multiqueue multiqueue;
+	multiqueue.nb_su_proc = 0;
+	multiqueue.nb_u_proc = 0;
+
+
+	for (p = FIRST_PROC; p <= LAST_PROC; p++) {
+		if(p->state != PROC_READY)
+			continue;
+
+		if (IS_SUPERUSER(p)) {
+			multiqueue.super_user_proc[multiqueue.nb_su_proc] = p;
+			multiqueue.nb_su_proc++;
+		} else {
+			multiqueue.user_proc[multiqueue.nb_u_proc] = p;
+			multiqueue.nb_u_proc++;
+		}
 	}
+
+	next = IDLE;
+
+	struct process **proc_list;
+	unsigned proc_list_size;
+
+	if (multiqueue.nb_su_proc != 0) {
+		proc_list = multiqueue.super_user_proc;
+		proc_list_size = multiqueue.nb_su_proc;
+	} else {
+		proc_list = multiqueue.user_proc;
+		proc_list_size = multiqueue.nb_u_proc;
+	}
+
+	for (unsigned i = 0; i < proc_list_size; i++) {
+		if (proc_list[i]->counter > next->counter) {
+			next->counter++;
+			next = proc_list[i];
+		} else {
+			proc_list[i]->counter++;
+		}
+
+	}
+
 	
 	/* Switch to next process. */
 	next->priority = PRIO_USER;
