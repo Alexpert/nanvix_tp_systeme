8a9b607a096ac3262606662f9dd65ecf48a2b1ec
GUIVARCH <alan.guivarch.98860@gmail.com>
2020-02-19 08:24:36 +0100

    WIP

--- include/sys/sem.h
+++ include/sys/sem.h
@@ -1,21 +1,23 @@
 /*
  * Copyright(C) 2011-2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
- * 
+ *
  * This file is part of Nanvix.
- * 
+ *
  * Nanvix is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Nanvix is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <nanvix/pm.h>
 
 #ifndef SEM_H_
 #define SEM_H_
@@ -29,9 +31,11 @@
 	#define IPC_RMID 3 /**< Destroys a semaphore.            */
 	/**@}*/
 
+	#define SEM_PROC_MAX 32
+	#define SEM_MAX 64
+
 	/* Forward definitions. */
 	extern int semget(unsigned);
 	extern int semctl(int, int, int);
 	extern int semop(int, int);
-
 #endif /* SEM_H_ */
---
+++ src/kernel/sys/sem.c
@@ -0,0 +1,100 @@
+#include <sys/sem.h>
+
+
+
+struct semaphore {
+ 	unsigned key;
+	unsigned int id;
+	unsigned int value;
+	unsigned int nb_proc;
+	struct process **processes;
+
+};
+
+ static struct semaphore sem_list[SEM_MAX];
+ static unsigned int nb_sem = 0;
+
+PUBLIC int sys_semget(unsigned key) {
+  for (int i = 0; i < SEM_MAX; i ++) {
+    if (sem_list[i].key == key) {
+      return sem_list[i].id;
+    }
+  }
+
+  sem_list[nb_sem].key = key;
+  sem_list[nb_sem].id = nb_sem;
+
+  nb_sem ++;
+  return sem_list[nb_sem-1].id;
+
+}
+
+PUBLIC int sys_semctl(int semid, int cmd, int val) {
+  if (val < 0)
+    return -1;
+  if (semid < 0 || semid > SEM_MAX - 1)
+    return -1;
+  if (cmd != GETVAL && cmd != SETVAL && cmd != IPC_RMID)
+     return -1;
+
+  unsigned i = 0;
+  while (i < nb_sem && sem_list[i].id != i)
+    i++;
+
+  if (i == nb_sem)
+    return -1;
+
+  int ret = 0;
+  switch (cmd) {
+    case GETVAL:
+      ret = sem_list[i].value;
+      break;
+    case SETVAL:
+      sem_list[i].value = val;
+      break;
+    case IPC_RMID:
+      while (i < nb_sem - 1) {
+        sem_list[i].key = sem_list[i + 1].key;
+        sem_list[i].id = sem_list[i + 1].id;
+        sem_list[i].value = sem_list[i + 1].value;
+        sem_list[i].nb_proc = sem_list[i + 1].nb_proc;
+        sem_list[i].processes = sem_list[i + 1].processes;
+        i++;
+      }
+      nb_sem --;
+      break;
+    default:
+      ret = -1;
+  }
+
+  return ret;
+}
+
+PUBLIC int sys_semop(int semid, int op) {
+  if (semid < 0 || semid > SEM_MAX - 1)
+    return -1;
+
+  unsigned i = 0;
+  while (i < nb_sem && sem_list[i].id != i)
+    i++;
+
+  if (i == nb_sem)
+    return -1;
+
+  struct semaphore *current_sem = &sem_list[i];
+  if (op < 0) {
+    if (current_sem->value > 0) {
+      current_sem->value--;
+    } else {
+      current_sem->value++;
+      //sleep
+    }
+  } else {
+    if (current_sem->value == 0 && current_sem->nb_proc > 0) {
+      //wakeup fifo
+    } else {
+      current_sem->value++;
+    }
+  }
+  return -1;
+}
